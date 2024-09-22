bc7e38ff14e0a9baec9e6a29d8364a92e73ccb45
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-09-22 20:22:20 +0100

    update

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Minimum Operations"""
 
+
 def minOperations(n):
+    """If n is impossible to achieve, return 0"""
     if n <= 1:
         return 0
 
     operations = 0
     divisor = 2
 
-    # Factorize n by dividing it by its smallest factors
+    """Factorize n by dividing it by its smallest factors"""
     while n > 1:
         while n % divisor == 0:
             operations += divisor
