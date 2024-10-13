9db12c0e446912a0ec70f0ba3678a4a6fe1f1166
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-13 17:17:46 +0100

    update

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total_size = 0
+    status_codes = {"200": 0,
+                    "301": 0,
+                    "400": 0,
+                    "401": 0,
+                    "403": 0,
+                    "404": 0,
+                    "405": 0,
+                    "500": 0}
+    line_count = 1
+
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """Print the metrics"""
+        print("File size: {}".format(total_size))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print("{}: {}".format(key, status_codes[key]))
+
+    try:
+        for line in sys.stdin:
+            total_size += parse_line(line)
+            if line_count % 10 == 0:
+                print_stats()
+            line_count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
